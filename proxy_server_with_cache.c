5eb9df824b9676bdb4c4330864f1ac00554b7e24
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-14 21:59:04 +0530

    Improve - not found in cache log

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -521,7 +521,7 @@ cache_element *find(char* url){
             site = site->next;
         }
     } else {
-        printf("URL not found");
+        printf("URL not found in cache");
     }
     temp_lock_val = pthread_mutex_unlock(&lock);
     printf("Lock is unlocked");
