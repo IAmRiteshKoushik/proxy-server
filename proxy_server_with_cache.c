a14de36230f27ad2a3434b8614ecb4131e089d13
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-14 18:11:27 +0530

    Handle incoming requests

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,245 @@
+#include "proxy_parse.h"
+
+#include <asm-generic/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <error.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_CLIENTS 10
+#define MAX_BYTES 4096    // bytes allocation space - 4KB
+
+typedef struct cache_element cache_element ;
+
+// Implementing the cache element for LRU cache (time-based)
+struct cache_element {
+    char* data;
+    int len;
+    char* url;
+    time_t lru_time_based;
+
+    // We need a linked-list to access corresponding cache elements
+    cache_element* next; 
+};
+
+cache_element* find(char* url);
+int add_cache_element(char* data, int size, char* url);
+void remove_cache_element();
+
+int port_number = 8080;
+int proxy_socketId;
+
+// Creating new thread for each socket connection with client
+// 1. Defining max-limit = 10 (array containig thread-ids)
+pthread_t tid[MAX_CLIENTS];
+
+// 2. LRU cache is a shared resource. When multiple threads access it there 
+// can be a race condition. Hence, we setup a lock.
+pthread_mutex_t lock;
+
+// 3. As we have setup a limit of 10 client connections, we might run out of 
+// client connections. In which case, we need to hold the further incoming 
+// requests till a connection becomes free.
+sem_t semaphore;
+
+// Global HEAD for the linkedlist which contains the LRU cache
+cache_element* head;
+int cache_size;
+
+void *thread_fn(void *socketNew){
+    // Using a semaphore. If the value has become negative then it waits, 
+    // otherwise it proceeds.
+    sem_wait(&semaphore);
+    int p;
+    sem_getvalue(&semaphore, p);
+    printf("Semaphore value is %d\n", p);
+
+    // Making a pointer
+    int *t = (int*) socketNew;
+    // Getting the value out of the pointer
+    int socket = *t;
+
+    // Now that a thread + socket has been allocated to the client, he will 
+    // start sending bytes. We need to receive them.
+    int bytes_send_client, len;
+
+    char *buffer = (char*)calloc(MAX_BYTES, sizeof(char));
+    bzero(buffer, MAX_BYTES);
+    // Params - 
+    bytes_send_client = recv(socket, buffer, MAX_BYTES, 0);
+    
+    while(bytes_send_client > 0){
+        len = strlen(buffer);
+        // Any HTTP request ends with "\r\n\r\n", we need to compare substring
+        // and see if we have reached that spot or not. If no then we must 
+        // keep reading else break out of the loop
+        if (strstr(buffer, "\r\n\r\n") == NULL){
+            bytes_send_client = recv(socket, buffer + len, MAX_BYTES - len, 0);
+        } else {
+            break;
+        }
+    }
+
+    // Dynamically allocating this because sizeof-character tends to 
+    // differ from OS-to-OS so we cannot hardcode this value
+    char *tempReq = (char *)malloc(strlen(buffer) * sizeof(char) + 1);
+    for(int i = 0; i < strlen(buffer); i++){
+        tempReq[i] = buffer[i];
+    }
+    struct cache_element* temp = find(tempReq);
+
+    // if the element is found in LRU cache
+    if(temp != NULL){
+        int size = temp -> len / sizeof(char);
+        int pos = 0;
+        char response[MAX_BYTES];
+        while(pos < size){
+            bzero(response, MAX_BYTES);
+            for(int i = 0; i < MAX_BYTES; i++){
+                response[i] = temp -> data[i];
+                pos++;
+            }
+            send(socket, response, MAX_BYTES, 0);
+        }
+        printf("Data retrived from the cache");
+        printf("%s\n\n", response);
+    }
+    // if element is not found in LRU cache, then first check if the bytes
+    // send by the client is greater than 0 or not (managing invalid requests)
+    else if (bytes_send_client > 0){
+        len = strlen(buffer);
+        struct ParsedRequest* request = ParsedRequest_create();
+
+        if(ParsedRequest_parse(request, buffer, len) < 0){
+            printf("Parsing failed\n");
+        } else {
+            bzero(buffer, MAX_BYTES);
+
+            if(!strcmp(request -> method, "GET")){
+                if(request->host && 
+                    request->path && 
+                    checkHTTPversion(request->version) == 1){
+                    
+                    bytes_send_client = handle_request(socket, request, tempReq);
+                    if(bytes_send_client == -1){
+                        // Internal server error - due to main server
+                        sendErrorMessage(socket, 500);
+                    }
+                } else {
+                    // Internal server error - due to proxy server
+                    sendErrorMessage(socket, 500);
+                }
+            } else {
+                printf("This code does not support any method except GET\n");
+            }
+        }
+        // Free everything
+        ParsedRequest_destroy(request);
+    } else if (bytes_send_client == 0){
+        printf("Client is disconnected");
+    }
+    shutdown(socket, SHUT_RDWR);
+    close(socket);
+    
+}
+
+
+int main(int argc, char* argv[]){
+    int client_socketId, client_len;
+    // When we open a socket, it returns a descriptor (same as opening files)
+    struct sockaddr_in server_addr, client_addr;
+    // Semaphore - minVal = 0, maxVal = 10
+    sem_init(&semaphore, 0, MAX_CLIENTS);
+    // Initializing lock with NULL
+    pthread_mutex_init(&lock, NULL);
+
+    if(argv == 2){
+        // ./proxy 9090 (takes two args)
+        port_number = atoi(argv[1]);
+    } else {
+        printf("Too few arguments");
+        exit(1); // sys-call
+    }
+
+    printf("Starting Proxy server at port: %d\n", port_number);
+    proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+
+    // If socket creation fails, it returns a negative value and exit
+    if (proxy_socketId < 0) {
+        perror("Failed to create a socket\n");
+        exit(1);
+    }
+
+    int reuse = 1;
+    if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, 
+        (const char*)&reuse, sizeof(reuse)) < 0) {
+        perror("setSockOpt failed\n");
+    }
+    bzero((char*)&server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Port binding
+    if(bind(proxy_socketId, 
+            (struct sockaddr*)&server_addr, sizeof(server_addr) < 0)){
+        perror("Port is not available");
+        exit(1);
+    }
+    printf("Binding on port %d\n", port_number);
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+    if (listen_status < 0){
+        perror("Error in listening\n");
+        exit(1);
+    }
+
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1){
+        bzero((char *)&client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_socketId = accept(proxy_socketId, 
+                (struct sockaddr *)&client_addr, 
+                (socklen_t *)&client_addr
+        );
+        if(client_socketId < 0){
+            printf("Not able to connect");
+            exit(1);
+        } else {
+            Connected_socketId[i] = client_socketId;
+        }
+
+        struct sockaddr_in* client_pt = (struct sockaddr_in *)&client_addr;
+        // Extract the client address from whichever socket that was opened
+        struct in_addr ip_addr = client_pt -> sin_addr;
+        char str[INET_ADDRSTRLEN];
+        // The function converts the address from network format to presentation
+        // format. Returns null if system error occurs.
+        inet_ntop(AF_INET, &ip_addr, str, INET_ADDRSTRLEN);
+        printf("Client is connected with port number %d and ip address is %s\n",
+               ntohs(client_addr.sin_port), str);
+
+        // All connections are open and have been accepted by the client
+        // Provide a socket to the thread so that other clients can come and 
+        // then a new socket is created and given to them.
+        pthread_create(&tid[i], NULL, thread_fn, (void *)&Connected_socketId[i]);
+        i++;
+    }
+
+    // Deallocate the socket memory
+    close(proxy_socketId);
+    return 0;
+}
